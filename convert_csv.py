4f8a254c1ae7be1df94148ade7d2f8696092da42
ShawnHwang1 <shawnhwang1@gmail.com>
2023-09-07 11:20:44 -0700

    pushed running python scripts and updated latitude and longitude conversions for maps in other catalog tests

---
+++ convert_csv.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import glob
+
+def convert_csv_to_pickdf(input_file, output_file):
+    df = pd.read_csv(input_file)
+    print("initial df")
+    print(df)
+
+    picks_df = pd.DataFrame(columns=["id", "timestamp", "prob", "type"])
+    
+    # Initialize counters and checks
+    p_check = 0
+    s_check = 0
+    event_id = 0
+
+    for index, row in df.iterrows():
+        # Check for p-arrival time
+        if (row['p_arrival_time'] != 0 and p_check == 0):
+            picks_df = picks_df.append({
+                "id": event_id,
+                "timestamp": row["p_arrival_time"],
+                "prob": row["p_probability"],
+                "type": "p"
+            }, ignore_index=True)
+            p_check += 1
+            event_id += 1
+        # Check for s-arrival time
+        elif (row['s_arrival_time'] != 0 and s_check == 0):
+            picks_df = picks_df.append({
+                "id": event_id,
+                "timestamp": row["s_arrival_time"],
+                "prob": row["s_probability"],
+                "type": "s"
+            }, ignore_index=True)
+            s_check += 1
+            event_id += 1
+
+print(pick_df)
