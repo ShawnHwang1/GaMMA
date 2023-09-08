fcd30b4f9d73a2a3d5c2d47c1614c7e3ea3afe83
ShawnHwang1 <shawnhwang1@gmail.com>
2023-09-08 15:28:35 -0700

    update time subtraction

--- compare_cat.py
+++ compare_cat.py
@@ -1,5 +1,5 @@
 from math import radians, sin, cos, sqrt, atan2
-
+import pandas as pd
 def distanceCalc(lat1, lon1, lat2, lon2):
     # Function to calculate distance (in kilometers) between two points given their latitude and longitude
     R = 6371.0 
@@ -26,14 +26,15 @@ def compare(catalog1, catalog2):
     numInCat2 = 0
     
     for event1 in catalog1:
-        matchFound = false
+        matchFound = False
+        print(event1)
         for event2 in catalog2:
-            timeDiff = abs(event1['time'] - event2['time'])
+            timeDiff = abs(event1['time'] - event2['time']) #cant subtract 2 time strings
             distance = distanceCalc(event1['latitude'], event1['longitude'], event2['latitude'], event2['longitude'])
             
             if timeDiff <= 5 and distance <= 25:
                 matches+=1 #++ does not work in python 
-                matchFound = true
+                matchFound = True
                 break
         
         if not matchFound:
@@ -42,4 +43,14 @@ def compare(catalog1, catalog2):
     numInCat2 = len(cat2) - matches
     
     return matches, numInCat1, numInCat2
-        
+       
+
+catalog_file1 = "earthquake_catalog.txt"
+catalog_file2 = "earthquake_catalog2.txt"
+data1 = pd.read_csv('earthquake_catalog.txt',sep= "\t",usecols = ['time','longitude','latitude'])
+
+data2 = pd.read_csv('earthquake_catalog2.txt',sep= "\t", usecols = ['time','longitude','latitude'])
+[matches, numInCat1, numInCat2] = compare(data1, data2)
+print(matches)
+print(numInCat1)
+print(numInCat2)
